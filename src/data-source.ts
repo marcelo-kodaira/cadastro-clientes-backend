42872061087740944257a20c7c1ee1ae213b37db
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-02-01 19:26:39 -0300

    Fix: data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,7 +12,6 @@ const AppDataSource = new DataSource(
     {
         type: "postgres",
         host: process.env.HOST,
-        port: 5432,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
